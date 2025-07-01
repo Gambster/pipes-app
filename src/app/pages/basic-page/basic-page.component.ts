4a650f699316ded171d4f41684b8f0f71688f38c
Gambster <gamboa040200@gmail.com>
2025-06-30 23:14:22 -0600

    feat: add DatePipe functionality and update basic page to display various date formats

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,13 +1,30 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import {
+  DatePipe,
+  LowerCasePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
   nameLower = signal<string>('gerardo');
   nameUpper = signal<string>('GERARDO');
   fullName = signal<string>('geRarDo GAmBoa');
+
+  customDate = signal<Date>(new Date());
+
+  tickingDateEffec = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+  });
 }
