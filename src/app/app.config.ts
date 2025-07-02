1ed64aa5372390ab68f1e027608ba281a07c70bf
Gambster <gamboa040200@gmail.com>
2025-07-01 22:28:40 -0600

    feat: add localization support and update basic page to display current locale with language switch buttons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+import localEs from '@angular/common/locales/es-MX';
+import localFr from '@angular/common/locales/fr';
+
+registerLocaleData(localEs, 'es');
+registerLocaleData(localFr, 'fr');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'es',
+    },
+  ],
 };
