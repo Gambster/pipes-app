9f622a2b7484f1b10b9545f7c16598e38cebd403
Gambster <gamboa040200@gmail.com>
2025-07-01 22:55:05 -0600

    feat: enhance uncommon page with i18nSelectPipe and dynamic client switching

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Gerardo',
+  gender: 'male',
+  age: 25,
+  address: 'Durango, México',
+};
+const client2 = {
+  name: 'Paola',
+  gender: 'female',
+  age: 24,
+  address: 'Roma, Italia',
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent {}
+export default class UncommonPageComponent {
+  // i18n select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+}
