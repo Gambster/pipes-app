fbe7c4874cf38ca04fe10cec17605b45d12d2150
Gambster <gamboa040200@gmail.com>
2025-07-02 23:27:48 -0600

    feat: implement custom page with ToggleCase pipe and hero data display

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent {}
+export default class CustomPageComponent {
+  name = signal('Gerardo Gamboa');
+  upperCase = signal(true);
+
+  toggleCase() {
+    this.upperCase.update((prevValue) => !prevValue);
+  }
+
+  heroes = signal(heroes);
+}
